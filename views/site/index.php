a9380069a3572b799a4ddbdb3f1fc445aaa45c69
zulkarom <zulkarom@gmail.com>
2024-05-13 10:47:02 +0800

    home

--- views/site/index.php
+++ views/site/index.php
@@ -2,15 +2,22 @@
 
 /* @var $this yii\web\View */
 
+use yii\helpers\Html;
+
 $this->title = 'HOME - I-CREATE - The International Convention on Resourceful Entrepreneurs Achieving Tomorrow\'s Excellence';
 ?>
   <div class="pagetitle">
       <div align="center">
-      <img src="<?=Yii::getAlias('@web')?>/images/logo-sub.png" style="max-width:500px" />
+      <img src="<?=Yii::getAlias('@web')?>/images/logo-sub.png" style="max-width:500px" width="100%" />
       </div>
 
     </div><!-- End Page Title -->
 
-    <section class="section dashboard">
+    <div style="text-align:center" align="center"> <?=Html::a('<i class="bi bi-box-arrow-in-right"></i> Login',['/site/login'],['class' => 'btn btn-primary'])?> <?=Html::a('<i class="bi bi-card-list"></i> Register',['/site/register'],['class' => 'btn btn-success'])?></div>
+
+
+<br />
+
+    <section>
     The International Convention on Resourceful Entrepreneurs Achieving Tomorrow’s Excellence (I-CREATE) serves as an academic nexus, consolidating diverse entrepreneurial innovation initiatives involving six sub-programs. COMEI 3.0 cultivates entrepreneurial zeal among students via workshops and pitch sessions, while JFED nurtures franchise business expertise, facilitating dialogue with industry elites. AIFIF augments student understanding of finance through seminars and career expos, harmonizing educational and industrial demands. NEWeek instills practical entrepreneurship by enabling students to enact theoretical concepts. IMPACT fosters community engagement and problem-solving acumen, while RISE provides a platform for showcasing innovative business concepts. At I-CREATE, these programs converge, fostering interdisciplinary discourse, creativity, and advancing entrepreneurship scholarship for future leaders.
     </section>
