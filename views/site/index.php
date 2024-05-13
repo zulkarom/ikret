13826f77f0de16b0f83dd94ecc6d8fca2d860e9b
zulkarom <zulkarom@gmail.com>
2024-05-13 10:48:22 +0800

    justiy

--- views/site/index.php
+++ views/site/index.php
@@ -18,6 +18,6 @@ $this->title = 'HOME - I-CREATE - The International Convention on Resourceful En
 
 <br />
 
-    <section>
+    <section style="text-align:justify">
     The International Convention on Resourceful Entrepreneurs Achieving Tomorrow’s Excellence (I-CREATE) serves as an academic nexus, consolidating diverse entrepreneurial innovation initiatives involving six sub-programs. COMEI 3.0 cultivates entrepreneurial zeal among students via workshops and pitch sessions, while JFED nurtures franchise business expertise, facilitating dialogue with industry elites. AIFIF augments student understanding of finance through seminars and career expos, harmonizing educational and industrial demands. NEWeek instills practical entrepreneurship by enabling students to enact theoretical concepts. IMPACT fosters community engagement and problem-solving acumen, while RISE provides a platform for showcasing innovative business concepts. At I-CREATE, these programs converge, fostering interdisciplinary discourse, creativity, and advancing entrepreneurship scholarship for future leaders.
     </section>
