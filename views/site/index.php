6350502e2a2c776809b6180decdaf57e26ead66f
zulkarom <zulkarom@gmail.com>
2024-06-09 21:56:26 +0800

    programme book

--- views/site/index.php
+++ views/site/index.php
@@ -36,3 +36,10 @@ $this->title = 'HOME - I-CREATE - The International Convention on Resourceful En
     <section style="text-align:justify">
     The International Convention on Resourceful Entrepreneurs Achieving Tomorrow’s Excellence (I-CREATE) serves as an academic nexus, consolidating diverse entrepreneurial innovation initiatives involving six sub-programs. COMEI 3.0 cultivates entrepreneurial zeal among students via workshops and pitch sessions, while JFED nurtures franchise business expertise, facilitating dialogue with industry elites. AIFIF augments student understanding of finance through seminars and career expos, harmonizing educational and industrial demands. NEWeek instills practical entrepreneurship by enabling students to enact theoretical concepts. IMPACT fosters community engagement and problem-solving acumen, while RISE provides a platform for showcasing innovative business concepts. At I-CREATE, these programs converge, fostering interdisciplinary discourse, creativity, and advancing entrepreneurship scholarship for future leaders.
     </section>
+
+    <div style="text-align: center; margin-top:5px" align="center">
+    <a href="https://anyflip.com/nsaql/bhfa/" target="_blank"><img src="<?=Yii::getAlias('@web')?>/images/icreate-programme-book.png" style="max-width:125px" width="90%" /></a> 
+    <div> <a href="https://anyflip.com/nsaql/bhfa/" target="_blank">Programme Book</a> </div>
+
+  </div>
+    
