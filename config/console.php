1ea2dcbca845ff1008c93e343272829434b9bf39
Zul Karami <zulkarom@gmail.com>
2026-04-04 19:08:01 +0800

    add class_exists checks for dev modules and remove db/db.php file
    - Wrap gii and debug module configurations with class_exists checks in console.php and web.php
    - Delete db/db.php configuration file

--- config/console.php
+++ config/console.php
@@ -41,18 +41,22 @@ $config = [
 
 if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-    ];
+    if (class_exists('yii\gii\Module')) {
+        $config['bootstrap'][] = 'gii';
+        $config['modules']['gii'] = [
+            'class' => 'yii\gii\Module',
+        ];
+    }
     // configuration adjustments for 'dev' environment
     // requires version `2.1.21` of yii2-debug module
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => 'yii\debug\Module',
-        // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
-    ];
+    if (class_exists('yii\debug\Module')) {
+        $config['bootstrap'][] = 'debug';
+        $config['modules']['debug'] = [
+            'class' => 'yii\debug\Module',
+            // uncomment the following to add your IP if you are not connecting from localhost.
+            //'allowedIPs' => ['127.0.0.1', '::1'],
+        ];
+    }
 }
 
 return $config;
